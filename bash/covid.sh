b7d330a51f00df6e95a9203ba4d6c2275f94c641
MrReenRoast <ryanrofe@gmail.com>
2023-09-09 10:13:59 -0400

    Lab 2

---
+++ bash/covid.sh
@@ -0,0 +1,7 @@
+DATA=$(curl https://api.covidtracking.com/v1/us/current.json)
+POSITIVE=$(echo $DATA | jq '.[0].positive')
+NEGATIVE=$(echo $DATA | jq '.[0].negative')
+DEATHS=$(echo $DATA | jq '.[0].death')
+HOSPITALIZED=$(echo $DATA | jq '.[0].hospitalized')
+TODAY=$(date)
+echo "On $TODAY, there were $POSITIVE positive COVID cases, $NEGATIVE negative tests, $DEATHS deaths, and $HOSPITALIZED hospitalized."
