0315620296b8a04071d2b264c01790af559a5730
MrReenRoast <ryanrofe@gmail.com>
2023-09-14 17:55:27 -0400

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,7 +1,19 @@
 ﻿function getIP{
     (Get-NetIPAddress).IPv4Address | Select-String "192*"
 }
-write-host(getIP)
+function getUSER{
+    (Get-LocalUser).Name | Select-String "Administrator"
+}
+function getHOST{
+    hostname
+}
+function getDATE{
+    Get-Date -Format "dddd MM/dd/yyyy"
+}
 $IP = getIP
-
-Write-Host("This machine's IP is $IP")
+$USER = getUSER
+$HOSTNAME = getHOST
+$VERSION = $HOST.Version.Major
+$DATE = getDATE
+$BODY = Write-Host("This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. PowerShell Version $VERSION. Today's Date is $DATE.")
+Send-MailMessage -To "roferr@mail.uc.edu" -From "somesoobstuufwithreasons@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
